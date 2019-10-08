5366ebebcd8334dc022227657a083154a9f6c097
James Schaffer <jas.schaffer@gmail.com>
2019-10-08 16:03:38 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,17 +1,20 @@
-def generate_number
-  lower = 1
-  upper = 6
-  
-  numbers = {
-    :goal => rand(lower..upper),
-    :lower => lower,
-    :upper => upper
-  }
-  
+def generate_goal
+  rand(1..6)
 end
 
+# def generate_number
+#  lower = 1
+#  upper = 6
+#  
+#  numbers = {
+#    :goal => rand(lower..upper),
+#    :lower => lower,
+#    :upper => upper
+#  }
+# end
+
 def prompt_user(numbers)
-#  puts "Guess a number between #{numbers[:lower]} and #{numbers[:upper]}."
+  puts "Guess a number between #{numbers[:lower]} and #{numbers[:upper]}."
 end
 
 def capture_input 
@@ -20,8 +23,9 @@ end
 
 
 def run_guessing_game
-  numbers = generate_number
-  prompt_user(numbers)
+#  numbers = generate_number
+  goal = generate_goal
+#  prompt_user(numbers)
   input = capture_input
   
   # compare   # respond
