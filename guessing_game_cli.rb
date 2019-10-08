72acb490718e6f7d22d5a73ec530a887460a9c1f
James Schaffer <jas.schaffer@gmail.com>
2019-10-08 15:40:34 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -8,14 +8,21 @@ rand(1..6)
 
 def run_guessing_game
   # generate_number
-  goal = rand(1..6)
+  lower = 1
+  upper = 6
   
-  prompt_user
+  goal = rand(lower..upper)
+  
+  # prompt_user
+  puts "Guess a number between #{lower} and #{upper}."
   
   # capture_input 
   input = gets.chomp
   
-  compare
+  # compare   # respond
+  case input
+    when input == goal
+      
   
 end
 
